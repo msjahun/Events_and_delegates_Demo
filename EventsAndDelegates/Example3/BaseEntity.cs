3478d9f69f5760a980ed282b402de1096d1a1598
Musa Jahun <msjahun@live.com>
2019-06-03 01:10:59 +0200

    Added base entity to replicate repository pattern behaviour

---
+++ EventsAndDelegates/Example3/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsAndDelegates.Example3
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+    }
+}
